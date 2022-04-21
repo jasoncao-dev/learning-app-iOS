0707745219b152094141aec1e5570a1fb46d5cf8
Trung Cao <caot1@nku.edu>
2022-04-20 22:02:00 -0400

    Initial setup

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  Learning App
+//
+//  Created by Jason Cao on 4/20/22.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  Learning App
 //
 //  Created by Jason Cao on 4/20/22.
@@ -7,15 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
